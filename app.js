283b21ffe1f10a823e2f57d478a5cd038a7354a1
Emilie Ickart <emi.ickart@gmail.com>
2022-04-16 16:38:28 +0200

    Add styles

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(($) => {
     $('#add').on('click', () => {
         let value = $('input').val();
         if (value !== '') {
-            let element = $('<li></li>').text(value);
+            let element = $("<li class='list-item'></li>").text(value);
             $(element).append("<button class='removeButton'>X</button>");
             $('#myList').append(element);
             $('input').val('');
